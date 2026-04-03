756c1cb88a3b2d943351e495e3b29c5f49c62506
lbarbier22 <barbier.lucas08@gmail.com>
2026-04-03 14:30:02 +0200

    feat : add Observer pattern with Event Manager

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+import EventManager from "./EventManager.js";
 import NotificationFactory from "./Notification/NotificationFactory.js";
 
-/**
- * Je dois faire :
- * - 1 notification email pour les nouvelles commandes
- * - 1 notification email pour les paiements refusés
- * - 1 notification sms pour les colis expédiés
- * - 1 notification interne pour les commandes importantes (genre montant > 1000€)
- */
+const manager = new EventManager();
 
-const mail = NotificationFactory.create('email', 'mail@mail.fr')
-const sms = NotificationFactory.create('sms', '+33676767676')
-const internal = NotificationFactory.create('interne', 'logistique')
+manager.subscribe('newCommand', NotificationFactory.create('email', 'mail@mail.fr'));
+manager.subscribe('PaymentRefused', NotificationFactory.create('email', 'mail@support.fr'));
+manager.subscribe('PackageSend', NotificationFactory.create('sms', '+33676767676'));
+manager.subscribe('ImportantPackage', NotificationFactory.create('interne', 'logistique'));
 
-mail.send("1")
-sms.send("2")
-internal.send("3")
+manager.notify('newCommand', 'Commande #1 confirmée');
+manager.notify('PaymentRefused', 'Échec paiement commande #2');
+manager.notify('PackageSend', 'Colis #1 expédié');
+manager.notify('ImportantPackage', 'Commande #3 : montant 1200€');
